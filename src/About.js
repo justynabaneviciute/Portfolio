5a617b9d31b805d88a73de0d908024cf85fb4bfe
justynabaneviciute <justyna.baneviciute@gmail.com>
2026-06-08 22:46:43 +0300

    taisymas

--- src/About.js
+++ src/About.js
@@ -5,31 +5,32 @@ function About() {
     <section id="apie" className="section about-section reveal-section">
       <div className="about-container">
         <div className="about-text">
-     <h2>APIE MANE</h2>
+          <h2>APIE MANE</h2>
 
-  <p >
-  Esu Justyna Banevičiūtė – multimedijos ir kompiuterinio dizaino studentė VILNIUS TECH universitete.
-  </p>
+          <p className="intro">Esu Justyna Banevičiūtė.</p>
 
-  <p>
-    Domiuosi grafiniu dizainu, vizualine komunikacija ir motion graphics.
-  </p>
+          <p>
+            Studijuoju Multimediją ir kompiuterinį dizainą Vilnius Tech
+            universitetas (III kursas).
+          </p>
 
-  <p>
-    Kuriu vizualinius sprendimus, kuriuose svarbus estetinis vaizdas ir aiškus informacijos perteikimas.
-  </p>
+          <p>
+            Kuriu vizualiai patrauklius dizaino sprendimus, kuriuose dera
+            estetika ir aiškus informacijos perteikimas.
+          </p>
 
-  <p>
-    Dirbu su Adobe Creative Cloud, Figma ir Blender, nuolat tobulinu savo įgūdžius bei plečiu žinias dizaino srityje.
-  </p>
-</div>
+          <p>
+            Dirbu su Adobe Photoshop, Illustrator ir Figma, nuolat tobulėju
+            kūrybinėje srityje bei plečiu savo žinias.
+          </p>
+        </div>
 
         <div className="about-right">
           <div className="blob">
             <img className="avatar-full" src="/avatar.png" alt="avatar" />
           </div>
 
-          <p className="role">Graphic & Motion Design</p>
+          <p className="role">Motion Designer</p>
 
           <div className="tools-icons">
          <img src="/ae.png" alt="ae" />
